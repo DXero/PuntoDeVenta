d09702c83dd532c62491719b2030f63c2801fc09
UNICAES <UNICAES@DESKTOP-J7M27OS>
2018-09-12 13:26:45 -0700

    Test commit

--- PuntoDeVenta/Form1.Designer.cs
+++ PuntoDeVenta/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(642, 450);
+            this.ClientSize = new System.Drawing.Size(618, 431);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
