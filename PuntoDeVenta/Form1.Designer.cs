fa3ba68f850c89950d4dab7b564eb27e88278a13
UNICAES <UNICAES@DESKTOP-5DM5TUL>
2018-09-12 13:19:57 -0700

    se le cambio el nombre  al formulario

--- PuntoDeVenta/Form1.Designer.cs
+++ PuntoDeVenta/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(642, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "login";
             this.ResumeLayout(false);
 
         }
