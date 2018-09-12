3f68a20384d20c54a6e00c5415db8312e1345612
UNICAES <UNICAES@DESKTOP-5DM5TUL>
2018-09-12 14:01:11 -0700

    agrege el diseño del formulario de Clientes

---
+++ PuntoDeVenta/Clientes/cliente form.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace PuntoDeVenta.Clientes
+{
+    partial class cliente_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.textApellidos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textFecha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textCorreo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textDui = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textNit = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboTipoUsuario = new System.Windows.Forms.ComboBox();
+            this.buttonAceptar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres";
+            // 
+            // textNombre
+            // 
+            this.textNombre.Location = new System.Drawing.Point(203, 68);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(100, 22);
+            this.textNombre.TabIndex = 1;
+            // 
+            // textApellidos
+            // 
+            this.textApellidos.Location = new System.Drawing.Point(203, 132);
+            this.textApellidos.Name = "textApellidos";
+            this.textApellidos.Size = new System.Drawing.Size(100, 22);
+            this.textApellidos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellidos";
+            // 
+            // textFecha
+            // 
+            this.textFecha.Location = new System.Drawing.Point(203, 198);
+            this.textFecha.Name = "textFecha";
+            this.textFecha.Size = new System.Drawing.Size(100, 22);
+            this.textFecha.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha de nacimiento";
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Location = new System.Drawing.Point(203, 275);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(100, 22);
+            this.textTelefono.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 275);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefono";
+            // 
+            // textCorreo
+            // 
+            this.textCorreo.Location = new System.Drawing.Point(470, 68);
+            this.textCorreo.Name = "textCorreo";
+            this.textCorreo.Size = new System.Drawing.Size(121, 22);
+            this.textCorreo.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Correo";
+            // 
+            // textDui
+            // 
+            this.textDui.Location = new System.Drawing.Point(470, 132);
+            this.textDui.Name = "textDui";
+            this.textDui.Size = new System.Drawing.Size(121, 22);
+            this.textDui.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(349, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "DUI";
+            // 
+            // textNit
+            // 
+            this.textNit.Location = new System.Drawing.Point(470, 198);
+            this.textNit.Name = "textNit";
+            this.textNit.Size = new System.Drawing.Size(121, 22);
+            this.textNit.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(349, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "NIT";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(349, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 17);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Tipo de Usuario";
+            // 
+            // comboTipoUsuario
+            // 
+            this.comboTipoUsuario.FormattingEnabled = true;
+            this.comboTipoUsuario.Location = new System.Drawing.Point(470, 275);
+            this.comboTipoUsuario.Name = "comboTipoUsuario";
+            this.comboTipoUsuario.Size = new System.Drawing.Size(121, 24);
+            this.comboTipoUsuario.TabIndex = 16;
+            // 
+            // buttonAceptar
+            // 
+            this.buttonAceptar.Location = new System.Drawing.Point(203, 361);
+            this.buttonAceptar.Name = "buttonAceptar";
+            this.buttonAceptar.Size = new System.Drawing.Size(132, 51);
+            this.buttonAceptar.TabIndex = 17;
+            this.buttonAceptar.Text = "Aceptar";
+            this.buttonAceptar.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(470, 361);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(133, 51);
+            this.buttonCancelar.TabIndex = 18;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            // 
+            // cliente_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonAceptar);
+            this.Controls.Add(this.comboTipoUsuario);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textNit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textDui);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textCorreo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textTelefono);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textApellidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "cliente_form";
+            this.Text = "cliente_form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.TextBox textApellidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textCorreo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textDui;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textNit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboTipoUsuario;
+        private System.Windows.Forms.Button buttonAceptar;
+        private System.Windows.Forms.Button buttonCancelar;
+    }
+}
---
+++ PuntoDeVenta/Clientes/cliente form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta.Clientes
+{
+    public partial class cliente_form : Form
+    {
+        public cliente_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
