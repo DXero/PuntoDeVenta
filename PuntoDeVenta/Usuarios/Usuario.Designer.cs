e1d794899d8fbd20baae3bdf8f041c0b3f19a2ca
ArnoldLR <luisrivas21@hotmail.com>
2018-09-12 19:59:12 -0600

    Add button

--- PuntoDeVenta/Usuarios/Usuario.Designer.cs
+++ PuntoDeVenta/Usuarios/Usuario.Designer.cs
@@ -39,6 +39,7 @@
             this.TextTelefono = new System.Windows.Forms.TextBox();
             this.TextDui = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -135,18 +136,28 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(354, 222);
+            this.button1.Location = new System.Drawing.Point(124, 272);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 49);
             this.button1.TabIndex = 10;
             this.button1.Text = "Guardar";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(310, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 49);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Usuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.TextDui);
             this.Controls.Add(this.TextTelefono);
@@ -180,5 +191,6 @@
         private System.Windows.Forms.TextBox TextTelefono;
         private System.Windows.Forms.TextBox TextDui;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
