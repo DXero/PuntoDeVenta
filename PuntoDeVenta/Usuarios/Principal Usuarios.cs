0420aeb0031a3bd600779ce9e8b56916d057eceb
UNICAES <UNICAES@DESKTOP-NS8IRLK>
2018-09-17 14:12:40 -0600

    Pantalla Usuarios

---
+++ PuntoDeVenta/Usuarios/Principal Usuarios.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta.Usuarios
+{
+    public partial class Principal_Usuarios : Form
+    {
+        public Principal_Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Usuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
