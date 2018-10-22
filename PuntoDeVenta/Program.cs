0ef94e0c9406b9d7fc1a09a48176ed9dd551dbb6
Denny Alexis Herrera Marín <denny_herrera@live.com>
2018-10-22 14:31:16 -0600

    Actualizacion de Modelo DB

--- PuntoDeVenta/Program.cs
+++ PuntoDeVenta/Program.cs
@@ -31,7 +31,7 @@ namespace PuntoDeVenta
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Clientes.cliente_form());
+            Application.Run(new Productos.Productos_Ver());
         }
     }
 }
