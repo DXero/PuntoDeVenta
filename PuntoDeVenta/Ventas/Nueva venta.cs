0f5b0e613135eff15191ba1b2229ffa3a4443aa8
ArnoldLR <luisrivas21@hotmail.com>
2018-10-23 18:48:03 -0600

    Formulario venta

---
+++ PuntoDeVenta/Ventas/Nueva venta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta.Ventas
+{
+    public partial class Agregar_venta : Form
+    {
+        public Agregar_venta()
+        {
+            InitializeComponent();
+        }
+    }
+}
