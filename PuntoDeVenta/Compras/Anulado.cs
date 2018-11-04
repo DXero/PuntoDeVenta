49273d1e56d9ac9369e51abaa1f0e2f5cfe54183
Meibelyn <meibelyn7@gmail.com>
2018-11-03 22:45:35 -0600

    Validaciòn Anular Compras

--- PuntoDeVenta/Compras/Anulado.cs
+++ PuntoDeVenta/Compras/Anulado.cs
@@ -16,5 +16,46 @@ namespace PuntoDeVenta.Compras
         {
             InitializeComponent();
         }
+
+        private void textMotivo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char[] NoPermitir = { 'é', 'ý', 'ú', 'í', 'ñ', 'ó', 'á', 'ë', 'ÿ', 'ü', 'ï', 'ö', 'ä', 'ê', 'û', 'î', 'ô', 'â', '-', '_', '^', '[', ']', '{', '}', '/' };
+            char[] Permitir = { '$', '%', '.', ',' };
+
+            //Validamos si el carácter es un valor permitido, si es falso entramos
+            if (!Permitir.Contains(e.KeyChar))
+            {
+                //Validamos que el valor sea un carácter no permitido
+                if (NoPermitir.Contains(e.KeyChar))
+                {
+                    MessageBox.Show("Carácter no permitido.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    e.Handled = true;
+                    return;
+                }
+                else
+                {
+                    //Si no es un carácter no permitido validamos que sea letra y numero o la tecla back
+                    if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && !(char.IsNumber(e.KeyChar)))
+                    {
+                        MessageBox.Show("Solo se permiten letras y números", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        e.Handled = true;
+                        return;
+                    }
+                }
+               
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textMotivo.Text != "" )
+            {
+          
+
+            }
+                else MessageBox.Show("Faltan campos por llenar");
+            
+        }
     }
 }
